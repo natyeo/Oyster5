ffec79ee78a315727273cfdf36ffd7ad138266a6
Ingrid Bjarman <ingridbjarman@Makerss-MacBook-Pro-6.local>
2019-10-21 14:28:44 +0100

    Step 5 - Top up method implemented

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -5,4 +5,8 @@ describe Oystercard do
   it 'checks that a new card has a balance of 0' do
     expect(oystercard.balance).to eq 0
   end
+  it { is_expected.to respond_to(:top_up).with(1).arguments }
+  it 'increments the balance by the amount passed as argument' do
+    expect{ subject.top_up(1) }.to change{ subject.balance }.by 1
+  end
 end
