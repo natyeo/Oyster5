65f4e38d40326a4cc9c42f294f616770958cf72a
Ingrid Bjarman <ingridbjarman@Makerss-MacBook-Pro-6.local>
2019-10-21 14:17:24 +0100

    Step 3-4 - refactoring, added balance

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,2 +1,8 @@
-describe 'oystercard' do
+require 'oystercard'
+
+describe Oystercard do
+  subject(:oystercard) { described_class.new }
+  it 'checks that a new card has a balance of 0' do
+    expect(oystercard.balance).to eq 0
+  end
 end
